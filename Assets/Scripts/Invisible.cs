e629fefe09046a9578d3b4b63e4c49ac7ef93bdc
ryan2134 <ryancheng2134@gmail.com>
2018-10-06 05:51:01 +1000

    added towers, projectile prototype, semi collision detection

---
+++ Assets/Scripts/Invisible.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invisible : MonoBehaviour {
+    public GameObject Obj;
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        MeshRenderer m = Obj.GetComponent<MeshRenderer>();
+        m.enabled = false;
+    }
+}
