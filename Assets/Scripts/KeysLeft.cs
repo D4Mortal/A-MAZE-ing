c0c69489fff5a433c564ca5b124fc5968cbb677b
Daniel Hao <danielhao461997@gmail.com>
2018-10-15 19:48:40 +1100

    added keys count to ui, added evaluation document

---
+++ Assets/Scripts/KeysLeft.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeysLeft : MonoBehaviour {
+
+    private int keyCount;
+    public keyManager keyManager;
+    public Text KeyCountText;
+    // Use this for initialization
+    void Start () {
+
+        this.keyManager = GameObject.Find("KeyManager").GetComponent<keyManager>();
+        keyCount = keyManager.keys;
+        KeyCountText.text = "Keys left: " + keyCount.ToString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        keyCount = keyManager.keys;
+        KeyCountText.text = "Keys left: " + keyCount.ToString();
+    }
+}
