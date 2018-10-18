11256d16baf9008c1ee0b0c13600b74f16481e5e
Raymond Sun <raymonds1@student.unimelb.edu.au>
2018-10-18 16:38:00 +1100

    Add doors and end to tutorial

---
+++ Assets/Scripts/OpenDoorTutorial.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoorTutorial : MonoBehaviour {
+
+    public Animator hingeAnimator;
+
+    // Use this for initialization
+    void Start () {
+        hingeAnimator = GetComponent<Animator>();
+
+    }
+    
+    // Update is called once per frame
+    void Update () {
+
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("Collided");
+        if (col.gameObject.name == "Player")
+        {
+            hingeAnimator.SetBool("unlocked",true);
+        }
+            
+    
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+
+    }
+
+
+
+}
