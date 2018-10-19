4bcad36462fa82323bcea6b6c09c92c179c4721e
Daniel Hao <danielhao461997@gmail.com>
2018-10-19 18:22:18 +1100

    added towers to complete the maze, fixed music bug, removed unused script

--- Assets/Scripts/ConsistentMusic.cs
+++ Assets/Scripts/ConsistentMusic.cs
@@ -7,7 +7,11 @@ public class ConsistentMusic : MonoBehaviour {
     // Use this for initialization
     private void Awake()
     {
-        GameObject gameObject = GameObject.FindGameObjectWithTag("Music");
-        DontDestroyOnLoad(gameObject);
+        GameObject[] gameObject = GameObject.FindGameObjectsWithTag("Music");
+        if (gameObject.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
     }
 }
